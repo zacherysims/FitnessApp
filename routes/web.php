53b77b9e817c780a8760c5a29efa1ce20ec56fa8
zacherysims <zach.md.sims97@gmail.com>
2019-04-10 14:50:30 -0600

    imma stupid

--- routes/web.php
+++ routes/web.php
@@ -47,3 +47,11 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
